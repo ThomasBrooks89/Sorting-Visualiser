052045d1463279ceda32d3479c5c9d7578ab2d6d
Thomas Brooks <tsjb89@gmail.com>
2026-03-03 10:22:03 +0000

    tidy up some test code

--- pygame_gui.py
+++ pygame_gui.py
@@ -1,8 +1,4 @@
 import pygame
-import sys
-
-pygame.init()
-font = pygame.font.Font(None, 32)
 
 class Button():
     width = 180
@@ -57,76 +53,3 @@ def draw_plus(surface, colour, size, x, y, width):
     offset = size // 2
     pygame.draw.line(surface, colour, (x - offset, y), (x + offset, y), width)
     pygame.draw.line(surface, colour, (x, y - offset), (x, y + offset), width)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    WIDTH = 2000
-    HEIGHT = 1200
-    FPS = 60
-    BG_COLOR = (18, 18, 18)
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("+x goes right, +y goes down")
-    clock = pygame.time.Clock()
-    running = True
-    font = pygame.font.Font(None, 15)
-
-    def draw_plus(surface, colour, size, x, y, width):
-        offset = size // 2
-        pygame.draw.line(surface, colour, (x - offset, y), (x + offset, y), width)
-        pygame.draw.line(surface, colour, (x, y - offset), (x, y + offset), width)
-
-        text_surface = font.render(f"{x}, {y}", True, colour)
-        surface.blit(text_surface, (x+8, y+10))
-        
-
-
-    def draw(surface):
-        surface.fill(BG_COLOR)
-        x, y = 0, 0
-
-        while x <= WIDTH:
-            while y <= HEIGHT:
-                draw_plus(surface, "white", 20, x, y, 2)
-                y += 100
-            y = 0
-            x += 100
-
-    btn = Button(screen, 750, 250, "poo", font)
-
-    while running:
-        dt = clock.tick(FPS) / 1000  # seconds since last frame
-
-        
-
-        # --- EVENTS ---
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            # keyboard example
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-
-        draw(screen)
-        btn.draw()
-        pygame.display.flip()
-
-
-    pygame.quit()
-    sys.exit()
